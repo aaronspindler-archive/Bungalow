48c7dadee13fb98838786e1bc8468a0f3f97392c
Aaron <aaron@xnovax.net>
2019-10-23 14:29:05 -0400

    Basic api with router and viewset

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import SimpleRouter
+from .views import HouseViewSet
+
+urlpatterns = [
+]
+
+router = SimpleRouter()
+router.register('houses', HouseViewSet, base_name='houses')
+urlpatterns += router.urls
